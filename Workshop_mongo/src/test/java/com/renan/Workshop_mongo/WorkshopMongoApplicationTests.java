ffb7302b971217334003315f89f6c2d82298b497
Renan <renan@gmail.com>
2023-08-19 05:37:51 -0300

    Add
    Signed-off-by: Renan <renan@gmail.com>

---
+++ Workshop_mongo/src/test/java/com/renan/Workshop_mongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.Workshop_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
