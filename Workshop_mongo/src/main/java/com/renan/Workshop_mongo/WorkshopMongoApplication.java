002992743bc388a72c501f00324859d98a3e6249
Renan <renan_camaro@hotmail.com>
2023-08-19 05:42:23 -0300

    Test
    Signed-off-by: Renan <renan_camaro@hotmail.com>

--- Workshop_mongo/src/main/java/com/renan/Workshop_mongo/WorkshopMongoApplication.java
+++ Workshop_mongo/src/main/java/com/renan/Workshop_mongo/WorkshopMongoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkshopMongoApplication {
 
+	//App
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopMongoApplication.class, args);
 	}
